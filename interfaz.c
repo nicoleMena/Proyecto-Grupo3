7373f22d2887c9f6846c06e3e363ab665cbaf164
StevenAOP <stevenaop2001@gmail.com>
2024-06-13 23:36:01 -0600

    Se crean los archivos funciones_ig.c funciones_ig.h Makefile_interfaz para crear las funciones que van a darle vida a la interfaz así como la declaración de funciones y el make file para facilitar. El botón Géneros ya despliega los géneros que hay y deja seleccionar entre 1, 2 o 3 géneros

--- interfaz.c
+++ interfaz.c
@@ -1,13 +1,15 @@
 #include <gtk/gtk.h>
+#include "funciones_ig.h"
 
 int main(int argc, char *argv[]) {
     GtkWidget *window;
     GtkWidget *main_box;
     GtkWidget *menu_box;
-    GtkWidget *generos_buton;
+    GtkWidget *generos_button;
 
     gtk_init(&argc, &argv);
 
+    // Crear la ventana principal
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(window), "Movie Mentor");
     gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
@@ -22,18 +24,15 @@ int main(int argc, char *argv[]) {
     gtk_box_pack_start(GTK_BOX(main_box), menu_box, FALSE, FALSE, 10);
 
     // Se crea el botón "Géneros"
-    generos_buton = gtk_button_new_with_label("Géneros");
-    gtk_widget_set_size_request(generos_buton, 200, -1); 
-    gtk_box_pack_start(GTK_BOX(menu_box), generos_buton, FALSE, FALSE, 0);
+    generos_button = gtk_button_new_with_label("Géneros");
+    gtk_widget_set_size_request(generos_button, 200, -1); 
+    gtk_box_pack_start(GTK_BOX(menu_box), generos_button, FALSE, FALSE, 0);
+
+    // Conectar la señal del botón para abrir el diálogo
+    g_signal_connect(generos_button, "clicked", G_CALLBACK(al_hacer_clic_en_boton_genero), NULL);
 
     gtk_widget_show_all(window);
     gtk_main();
 
     return 0;
 }
-
-
-
-
-
-
