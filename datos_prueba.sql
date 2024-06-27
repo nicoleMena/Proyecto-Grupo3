5d9eb6f54f0653d4f4ba4cd1a739f637c03280df
Daniel Rojas <daniel.rojascanas@ucr.ac.cr>
2024-06-27 03:55:11 -0600

    Expansion base de datos

--- datos_prueba.sql
+++ datos_prueba.sql
@@ -4,22 +4,31 @@ INSERT INTO Director (nombre) VALUES ('Steven Spielberg');
 INSERT INTO Director (nombre) VALUES ('Christopher Nolan');
 INSERT INTO Director (nombre) VALUES ('Martin Scorsese');
 INSERT INTO Director (nombre) VALUES ('Ridley Scott');
+INSERT INTO Director (nombre) VALUES ('Tim Burton');  
+INSERT INTO Director (nombre) VALUES ('James Cameron');
+INSERT INTO Director (nombre) VALUES ('Hayao Miyazaki');
+INSERT INTO Director (nombre) VALUES ('John Carpenter');
 
--- Agregar estudios cinematograficos
+-- Agregar estudios cinematográficos
 INSERT INTO Estudio (nombre) VALUES ('Miramax');
 INSERT INTO Estudio (nombre) VALUES ('Universal Pictures');
 INSERT INTO Estudio (nombre) VALUES ('Warner Bros');
 INSERT INTO Estudio (nombre) VALUES ('Paramount Pictures');
 INSERT INTO Estudio (nombre) VALUES ('20th Century Fox');
+INSERT INTO Estudio (nombre) VALUES ('Disney'); 
+INSERT INTO Estudio (nombre) VALUES ('Studio Ghibli');
+INSERT INTO Estudio (nombre) VALUES ('20th Century Studios');
 
--- Agregar generos
+-- Agregar géneros
 INSERT INTO Genero (nombre) VALUES ('Comedia');
 INSERT INTO Genero (nombre) VALUES ('Drama');
 INSERT INTO Genero (nombre) VALUES ('Accion');
-INSERT INTO Genero (nombre) VALUES ('Ciencia Ficcion');
+INSERT INTO Genero (nombre) VALUES ('Ficcion');
 INSERT INTO Genero (nombre) VALUES ('Thriller');
+INSERT INTO Genero (nombre) VALUES ('Terror');
+INSERT INTO Genero (nombre) VALUES ('Animada');
 
--- Agregar peliculas
+-- Agregar películas
 INSERT INTO Pelicula (nombre, anio, director_id, estudio_id) VALUES 
 ('Pulp Fiction', 1994, 1, 1),
 ('Jurassic Park', 1993, 2, 2),
@@ -30,25 +39,34 @@ INSERT INTO Pelicula (nombre, anio, director_id, estudio_id) VALUES
 ('Gladiator', 2000, 5, 5),
 ('Alien', 1979, 5, 5),
 ('Inception', 2010, 3, 3),
-('Django Unchained', 2012, 1, 1);
+('Django Unchained', 2012, 1, 1),
+('The Nightmare Before Christmas', 1993, 6, 6), 
+('Coraline', 2009, 6, 6), 
+('Titanic', 1997, 7, 5),
+('Spirited Away', 2001, 8, 7),
+('The Thing', 1982, 9, 5),
+('Avatar', 2009, 7, 5),
+('My Neighbor Totoro', 1988, 8, 7),
+('Halloween', 1978, 9, 3);
 
--- Asignar generos a las películas
+-- Asignar géneros a las películas
 INSERT INTO PeliculaGenero (pelicula_id, genero_id) VALUES
-(3, 3), -- Se le asigna uno o mas generos a cada pelicula
-(4, 1),  
-(4, 2), 
-(5, 2), 
-(6, 1),  
-(6, 2), 
-(7, 1),  
-(7, 4),  
-(8, 2),  
-(8, 5),  
-(9, 1),  
-(9, 2),  
-(10, 1), 
-(10, 4), 
-(11, 1), 
-(11, 4),
-(12, 1), 
-(12, 2); 
+(1, 5), -- Se le asigna uno o mas generos a cada pelicula
+(2, 2),
+(3, 2),
+(4, 5),
+(5, 3),
+(6, 1),
+(7, 4),
+(8, 6),
+(9, 4),
+(10, 1),
+(11, 7),  
+(12, 7),  
+(12, 6),  
+(13, 2),  
+(14, 7),  
+(15, 6),  
+(16, 4),  
+(17, 7),  
+(18, 6); 
